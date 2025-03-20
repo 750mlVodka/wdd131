d31162e05d812290b4952d424bdd3090e775b303
yamsir <yamsirgg@gmail.com>
2025-03-20 01:56:40 -0600

    hamburger nav without results :c

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuButton = document.getElementById("menuButton");
-    const navMenu = document.getElementById("navMenu");
+    const navMenuSmall = document.getElementById("navMenuSmall");
 
     menuButton.addEventListener("click", function() {
-        navMenu.classList.toggle("active");
-        menuButton.textContent = navMenu.classList.contains("active") ? "✖" : "☰";
+        navMenuSmall.classList.toggle("active");
+        menuButton.textContent = navMenuSmall.classList.contains("active") ? "✖" : "☰";
     });
 });

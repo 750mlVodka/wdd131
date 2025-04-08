95dc77c8ff06ee4bb0682f406df2ac55e6d52ab4
yamsir <yamsirgg@gmail.com>
2025-04-07 18:09:44 -0600

    js added

--- scripts/temples.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuButton = document.getElementById("menuButton");
-    const navMenuSmall = document.getElementById("navMenuSmall");
-
-    menuButton.addEventListener("click", function() {
-        navMenuSmall.classList.toggle("active");
-        menuButton.textContent = navMenuSmall.classList.contains("active") ? "✖" : "☰";
-    });
-});
